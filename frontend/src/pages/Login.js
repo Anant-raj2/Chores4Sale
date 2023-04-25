706cd3af6003dbd0ab965313e781737474b1116a
Anant-raj2 <122117953+Anant-raj2@users.noreply.github.com>
2023-04-25 12:33:30 -0500

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,28 @@
 import { useEffect, useState } from "react";
 import jwt_decode from "jwt-decode";
+import { createClient } from '@supabase/supabase-js'
 
 const Login = () => {
+  const supabaseUrl = 'https://nakokxtvynhsbxlxpzwx.supabase.co'
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ha29reHR2eW5oc2J4bHhwend4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI0NDA0MTksImV4cCI6MTk5ODAxNjQxOX0.Xci4_R1rqxh5pydTVXt8hhkZQb6e3QmYx6PdShpGJ1A'
+  const supabase = createClient(supabaseUrl, supabaseKey)
+
+  async function addUser(name, email){
+    
+    const { data, error } = await supabase
+    .from('users')
+    .insert([
+      { name: name },
+      { email: email },
+    ])
+
+  }
   const [user, setUser] = useState({});
   function handleCallbackResponse(response) {
     console.log("Encoded JWT ID token: " + response.credential);
     let userObject = jwt_decode(response.credential);
-    console.log(userObject);
-    setUser(userObject);
+    console.log(userObject.name);
+    addUser(userObject.name, userObject.email)
   }
 
   useEffect(() => {
