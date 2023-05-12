d7543acd1a3e4c3517853d19a97b13c793e3a3d6
Anant-raj2 <122117953+Anant-raj2@users.noreply.github.com>
2023-05-12 11:53:04 -0500

    Update AddJob.js

--- frontend/src/pages/AddJob.js
+++ frontend/src/pages/AddJob.js
@@ -6,13 +6,13 @@ const AddJob = () => {
       <form action="#" className="form">
         
         <div className="input-box address">
-          <label>Address</label>
-          <input type="text" placeholder="Enter street address" required />
-          <input type="text" placeholder="Enter street address line 2" required />
+          <label>Job</label>
+          <input type="text" placeholder="Enter Job Name" required />
+          <input type="text" placeholder="Enter Job Description" required />
+          <input type="text" placeholder="Enter Job Price" required />
+
           <div className="column">
-            <input type="text" placeholder="Enter your city" required />
-          </div>
-          <div className="column">
+            <input type="text" placeholder="Enter Address" required />
             <input type="text" placeholder="Enter your region" required />
             <input type="number" placeholder="Enter postal code" required />
           </div>
